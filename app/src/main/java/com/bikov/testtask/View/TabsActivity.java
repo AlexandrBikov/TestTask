ebce74f93964691ff1e1d89553d81426a436aa13
80802620Qq <Abbibbas723@gmail.com>
2019-01-09 18:26:15 +0300

    method for changing visibility of tabs

--- app/src/main/java/com/bikov/testtask/View/TabsActivity.java
+++ app/src/main/java/com/bikov/testtask/View/TabsActivity.java
@@ -10,38 +10,43 @@ import android.view.View;
 
 import com.bikov.testtask.R;
 
+import java.util.ArrayList;
+
 public class TabsActivity extends AppCompatActivity {
 
     private View accountView;
     private View mapView;
     private View listView;
+    private ArrayList<View> fragmentViews;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        //TODO method to change visibility
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_map:
-                    mapView.setVisibility(View.VISIBLE);
-                    accountView.setVisibility(View.INVISIBLE);
-                    listView.setVisibility(View.INVISIBLE);
+                    changeVisibility(mapView);
                     return true;
                 case R.id.navigation_account:
-                    mapView.setVisibility(View.INVISIBLE);
-                    accountView.setVisibility(View.VISIBLE);
-                    listView.setVisibility(View.INVISIBLE);
+                    changeVisibility(accountView);
                     return true;
                 case R.id.navigation_markers:
-                    mapView.setVisibility(View.INVISIBLE);
-                    accountView.setVisibility(View.INVISIBLE);
-                    listView.setVisibility(View.VISIBLE);
+                    changeVisibility(listView);
                     return true;
             }
             return false;
         }
     };
+
+    private void changeVisibility(View visible){
+        for(View view:fragmentViews){
+            if(view.getVisibility() == View.VISIBLE){
+                view.setVisibility(View.INVISIBLE);
+            }
+        }
+        visible.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +60,11 @@ public class TabsActivity extends AppCompatActivity {
         accountView = accountFragment.getView();
         mapView = mapFragment.getView();
         listView = listFragment.getView();
+
+        fragmentViews = new ArrayList<>(3);
+        fragmentViews.add(accountView);
+        fragmentViews.add(mapView);
+        fragmentViews.add(listView);
 
         accountView.setVisibility(View.INVISIBLE);
         listView.setVisibility(View.INVISIBLE);
